3e358267f91ff7357d19037f371894fdda688d33
fan-tech <fan.n.o.to.39@gmail.com>
2024-08-26 08:55:28 +0900

    軽微な修正

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
   this.classList.toggle("open");
   document.getElementById("menu").classList.toggle("open");
 });
+
+// メニュー内のリンクがクリックされたときにメニューを閉じる
+document.querySelectorAll("nav ul li a").forEach(function (link) {
+  link.addEventListener("click", function () {
+    document.getElementById("menu-toggle").classList.remove("open");
+    document.getElementById("menu").classList.remove("open");
+  });
+});
