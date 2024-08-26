c538614fa935af547abebe7867a4ea71bca0e619
fan-tech <fan.n.o.to.39@gmail.com>
2024-08-26 09:36:46 +0900

    ハンバーガーメニュー挙動修正

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-document.getElementById("menu-toggle").addEventListener("click", function () {
+document.querySelector(".menu-toggle").addEventListener("click", function () {
   this.classList.toggle("open");
-  document.getElementById("menu").classList.toggle("open");
+  document.querySelector("nav ul").classList.toggle("open");
 });
 
-// メニュー内のリンクがクリックされたときにメニューを閉じる
 document.querySelectorAll("nav ul li a").forEach(function (link) {
   link.addEventListener("click", function () {
-    document.getElementById("menu-toggle").classList.remove("open");
-    document.getElementById("menu").classList.remove("open");
+    // メニューがタップされたら、三本線に戻る
+    document.querySelector(".menu-toggle").classList.remove("open");
+    document.querySelector("nav ul").classList.remove("open");
   });
 });
